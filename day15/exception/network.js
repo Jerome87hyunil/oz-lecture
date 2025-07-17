e57261ca6db8e74f92cfef75e38ee899a428a2c3
Jerome87hyunil <Jerome87hyunil@users.noreply.github.com>
2025-07-17 10:14:22 +0900

    day 15

---
+++ day15/exception/network.js
@@ -0,0 +1,12 @@
+async function getPost(postNum) {
+  try {
+    let response = await fetch(
+      `https://jsonplaceholder.typicode.com/posts/${postNum}`
+    );
+    let data = await response.json();
+    console.log(data);
+  } catch (error) {
+    alert("에러: " + error.message);
+  }
+}
+getPost(102);
